76620156f3d331956a1420158188671d8d431a8b
vahram2292 <vahram2292@gmail.com>
2023-06-25 16:08:29 +0400

    refactoring, some todos for next phase of refactoring
    - add constant for some variables
    - remove link for manifest
    - some small refactoring on main ts
    - test on couple of 4000 images and assets

---
+++ js/constants.js
@@ -0,0 +1,5 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.CIRCLE_WIDTH_HALF = void 0;
+exports.CIRCLE_WIDTH_HALF = 42;
+//# sourceMappingURL=constants.js.map
